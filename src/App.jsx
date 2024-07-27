5a6a9afa0356ca5a53a1bcfbfe98e8d2db425b18
unknown <hardeyemo3@gmail.com>
2024-07-27 08:56:29 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -47,10 +47,3 @@ const App = () => {
 }
 
 export default App
-
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/hardeyemo/Shaby.git
-git push -u origin main
